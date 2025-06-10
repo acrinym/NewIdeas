208d3f0427a7d23651acd4d6049c0e6d3231994b
acrinym <acrinym@gmail.com>
2025-06-09 20:07:30 -0400

    Remove binary icon assets and implement plugin hot reload

---
+++ Cycloside/App.axaml.cs
@@ -0,0 +1,57 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using Cycloside.Plugins;
+using System;
+using System.IO;
+
+namespace Cycloside;
+
+public partial class App : Application
+{
+    private const string TrayIconBase64 = "AAABAAEAEBACAAEAAQCwAAAAFgAAACgAAAAQAAAAIAAAAAEAAQAAAAAAQAAAAAAAAAAAAAAAAgAAAAIAAAAAAP8A////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            var manager = new PluginManager(Path.Combine(AppContext.BaseDirectory, "Plugins"));
+            manager.LoadPlugins();
+            manager.StartWatching();
+
+            var iconData = Convert.FromBase64String(TrayIconBase64);
+            var trayIcon = new TrayIcon
+            {
+                Icon = new WindowIcon(new MemoryStream(iconData))
+            };
+
+            var menu = new NativeMenu();
+            var settingsItem = new NativeMenuItem("Settings");
+            settingsItem.Click += (_, _) =>
+            {
+                var win = new MainWindow();
+                win.Show();
+            };
+            var exitItem = new NativeMenuItem("Exit");
+            exitItem.Click += (_, _) =>
+            {
+                manager.StopAll();
+                desktop.Shutdown();
+            };
+
+            menu.Items.Add(settingsItem);
+            menu.Items.Add(new NativeMenuItemSeparator());
+            menu.Items.Add(exitItem);
+
+            trayIcon.Menu = menu;
+            trayIcon.IsVisible = true;
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
---
+++ Cycloside/Plugins/PluginManager.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+
+namespace Cycloside.Plugins;
+
+public class PluginManager
+{
+  private readonly List<IPlugin> _plugins = new();
+  private FileSystemWatcher? _watcher;
+  private readonly object _lock = new();
+
+    public string PluginDirectory { get; }
+
+  public PluginManager(string pluginDirectory)
+  {
+      PluginDirectory = pluginDirectory;
+  }
+
+  public void StartWatching()
+  {
+      if (_watcher != null)
+          return;
+
+      _watcher = new FileSystemWatcher(PluginDirectory, "*.dll")
+      {
+          EnableRaisingEvents = true,
+          NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName
+      };
+
+      _watcher.Created += (_, __) => ReloadPlugins();
+      _watcher.Changed += (_, __) => ReloadPlugins();
+      _watcher.Deleted += (_, __) => ReloadPlugins();
+  }
+
+  public void LoadPlugins()
+  {
+      if (!Directory.Exists(PluginDirectory))
+          Directory.CreateDirectory(PluginDirectory);
+
+        foreach (var dll in Directory.GetFiles(PluginDirectory, "*.dll"))
+        {
+            try
+            {
+                var asm = Assembly.LoadFrom(dll);
+                var types = asm.GetTypes().Where(t => typeof(IPlugin).IsAssignableFrom(t) && !t.IsAbstract);
+                foreach (var type in types)
+                {
+                    if (Activator.CreateInstance(type) is IPlugin plugin)
+                    {
+                        _plugins.Add(plugin);
+                        plugin.Start();
+      }
+  }
+
+  public void ReloadPlugins()
+  {
+      lock (_lock)
+      {
+          StopAll();
+          _plugins.Clear();
+          LoadPlugins();
+      }
+  }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to load plugin {dll}: {ex.Message}");
+            }
+        }
+    }
+
+  public void StopAll()
+  {
+      foreach (var p in _plugins)
+      {
+          try
+          {
+              p.Stop();
+          }
+          catch
+          {
+              // ignore
+          }
+      }
+
+      _watcher?.Dispose();
+      _watcher = null;
+  }
+}
