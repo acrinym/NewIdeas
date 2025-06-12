19e2789c66c17132b6102c91f08faa2b837d55e9
acrinym <acrinym@gmail.com>
2025-06-12 12:52:12 -0400

    Add built-in utilities

---
+++ Cycloside/Plugins/BuiltIn/DiskUsagePlugin.cs
@@ -0,0 +1,76 @@
+using Avalonia.Controls;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Cycloside.Plugins.BuiltIn;
+
+public class DiskUsagePlugin : IPlugin
+{
+    private Window? _window;
+    private TreeView? _tree;
+
+    public string Name => "Disk Usage";
+    public string Description => "Visualize disk usage";
+    public Version Version => new(0,1,0);
+    public Widgets.IWidget? Widget => null;
+
+    public async void Start()
+    {
+        _tree = new TreeView();
+        var button = new Button { Content = "Select Folder" };
+        button.Click += async (_, __) =>
+        {
+            var dlg = new OpenFolderDialog();
+            var path = await dlg.ShowAsync(_window);
+            if (!string.IsNullOrWhiteSpace(path))
+                LoadTree(path);
+        };
+
+        var panel = new StackPanel();
+        panel.Children.Add(button);
+        panel.Children.Add(_tree);
+
+        _window = new Window
+        {
+            Title = "Disk Usage",
+            Width = 500,
+            Height = 400,
+            Content = panel
+        };
+        ThemeManager.ApplyFromSettings(_window, "Plugins");
+        WindowEffectsManager.Instance.ApplyConfiguredEffects(_window, nameof(DiskUsagePlugin));
+        _window.Show();
+    }
+
+    private void LoadTree(string root)
+    {
+        var rootNode = BuildNode(new DirectoryInfo(root));
+        _tree!.Items = new[] { rootNode };
+    }
+
+    private static TreeViewItem BuildNode(DirectoryInfo dir)
+    {
+        long size = dir.GetFiles().Sum(f => f.Length);
+        foreach (var sub in dir.GetDirectories())
+            size += GetDirSize(sub);
+        var node = new TreeViewItem { Header = $"{dir.Name} ({size / 1024 / 1024} MB)" };
+        node.Items = dir.GetDirectories().Select(BuildNode).ToList();
+        return node;
+    }
+
+    private static long GetDirSize(DirectoryInfo dir)
+    {
+        long size = dir.GetFiles().Sum(f => f.Length);
+        foreach (var sub in dir.GetDirectories())
+            size += GetDirSize(sub);
+        return size;
+    }
+
+    public void Stop()
+    {
+        _window?.Close();
+        _window = null;
+        _tree = null;
+    }
+}
