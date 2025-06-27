438ad2b3325a2838d99125324e61cecd5a336f6c
acrinym <acrinym@gmail.com>
2025-06-26 23:10:20 -0400

    Fix build by adjusting package versions and event handler

--- Cycloside/Plugins/BuiltIn/Views/ClipboardManagerWindow.axaml.cs
+++ Cycloside/Plugins/BuiltIn/Views/ClipboardManagerWindow.axaml.cs
@@ -9,5 +9,16 @@ namespace Cycloside.Plugins.BuiltIn
             // This line is essential to load the UI defined in the .axaml file.
             InitializeComponent();
         }
+
+        private void ListBox_DoubleTapped(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            if (DataContext is ClipboardManagerPlugin vm && sender is ListBox lb && lb.SelectedItem is string text)
+            {
+                if (vm.EntrySelectedCommand.CanExecute(text))
+                {
+                    vm.EntrySelectedCommand.Execute(text);
+                }
+            }
+        }
     }
 }
