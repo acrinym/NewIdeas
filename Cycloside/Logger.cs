16314c53a0b05d218f93324dd74633db8a0f399e
acrinym <acrinym@gmail.com>
2025-06-30 18:57:30 -0400

    Update Logger.cs

--- Cycloside/Logger.cs
+++ Cycloside/Logger.cs
@@ -71,11 +71,4 @@ public static class Logger
         catch (TaskCanceledException) { }
         catch (AggregateException) { } // Can be thrown if the task faults, ignore on shutdown.
     }
-
-    /// <summary>
-    /// Gracefully shuts down the logger. No-op for current implementation.
-    /// </summary>
-    public static void Shutdown()
-    {
-    }
 }
